f3043f2161c602328dec8da0b70fad5a776e3d8a
huyidong <hydjay@163.com>
2016-05-22 16:59:03 +0800

    change

--- Public/config/dbconfig.inc.php
+++ Public/config/dbconfig.inc.php
@@ -3,14 +3,14 @@
 return array(
 	'DB_TYPE' => 'mysql',
 	'DB_HOST' => 'localhost',
-	'DB_NAME' => 'ruiblog',
+	'DB_NAME' => 'blog',
 	'DB_USER' => 'root',
-	'DB_PWD' => '',
-	'DB_PORT'  => '3306',
-	'DB_PREFIX' => 'iqishe_',
-	'RBAC_ROLE_TABLE' => 'iqishe_role',
-	'RBAC_USER_TABLE'=>'iqishe_role_user',
-	'RBAC_ACCESS_TABLE'=>'iqishe_access',
-	'RBAC_NODE_TABLE'=>'iqishe_node',
+	'DB_PWD' => '141872redis',
+	'DB_PORT'  => '3306'
+	'DB_PREFIX' => 'hyd_',
+	'RBAC_ROLE_TABLE' => 'hyd_role',
+	'RBAC_USER_TABLE'=>'hyd_role_user',
+	'RBAC_ACCESS_TABLE'=>'hyd_access',
+	'RBAC_NODE_TABLE'=>'hyd_node',
 );
 ?>
