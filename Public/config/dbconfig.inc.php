4902af2a0165920d1352a0494a9f1138b90933e6
huyidong <hydjay@163.com>
2016-05-24 09:40:38 +0800

    debug

--- Public/config/dbconfig.inc.php
+++ Public/config/dbconfig.inc.php
@@ -6,8 +6,8 @@ return array(
 	'DB_NAME' => 'blog',
 	'DB_USER' => 'root',
 	'DB_PWD' => '141872redis',
-	'DB_PORT'  => '3306'
-	'DB_PREFIX' => 'hyd',
+	'DB_PORT'  => '3306',
+	'DB_PREFIX' => 'hyd_',
 	'RBAC_ROLE_TABLE' => 'hyd_role',
 	'RBAC_USER_TABLE'=>'hyd_role_user',
 	'RBAC_ACCESS_TABLE'=>'hyd_access',
